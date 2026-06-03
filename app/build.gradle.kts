6a87340091341850f1162e67cc6db31fb8985387
ahmed-farhanur-rashid <ahmed.farhanur.rashid@gmail.com>
2026-06-04 03:54:58 +0600

    fix: add Google Material dependency version 1.12.0 to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,6 +100,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.extended)
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.navigation.compose)
 
     // Hilt DI
